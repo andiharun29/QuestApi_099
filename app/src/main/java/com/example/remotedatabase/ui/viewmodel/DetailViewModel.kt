d781b4413e1d82aac6671f7862d420079a62cc29
andiharun29 <andiharunalrasyid14@gmail.com>
2024-12-30 10:48:53 +0700

    Menambahkan ekstensi toDetailUiEvent untuk konversi Mahasiswa ke InsertUiEvent

---
+++ app/src/main/java/com/example/remotedatabase/ui/viewmodel/DetailViewModel.kt
@@ -0,0 +1,23 @@
+package com.example.remotedatabase.ui.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.remotedatabase.Repository.MahasiswaRepository
+import com.example.remotedatabase.model.Mahasiswa
+import kotlinx.coroutines.launch
+
+
+
+fun Mahasiswa.toDetailUiEvent(): InsertUiEvent{
+    return InsertUiEvent(
+        nim = nim,
+        nama = nama,
+        alamat = alamat,
+        jeniskelamin = jeniskelamin,
+        kelas = kelas,
+        angkatan = angkatan
+    )
+}
