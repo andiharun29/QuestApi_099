da9dbf37896eb76ca5ca95689fe2d30a90f31234
andiharun29 <andiharunalrasyid14@gmail.com>
2024-12-30 11:03:53 +0700

    Menambahkan HomeStatus untuk menangani tampilan status UI seperti loading, error, dan data berhasil

--- app/src/main/java/com/example/remotedatabase/ui/view/HomeView.kt
+++ app/src/main/java/com/example/remotedatabase/ui/view/HomeView.kt
@@ -47,6 +47,37 @@ import com.example.remotedatabase.ui.viewmodel.PenyediaViewModel
 
 
 
+@Composable
+fun HomeStatus(
+    homeUiState: HomeUiState,
+    retryAction: () -> Unit,
+    modifier: Modifier = Modifier,
+    onDeleteClick: (Mahasiswa) -> Unit = {},
+    onDetailClick: (String) -> Unit
+){
+    when (homeUiState){
+        is HomeUiState.Loading -> OnLoading(modifier = modifier.fillMaxSize())
+
+        is HomeUiState.Success ->
+            if(homeUiState.mahasiswa.isEmpty()){
+                return Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center){
+                    Text(text = "Tidak ada data Mahasiswa")
+                }
+            }else {
+                MhsLayout(
+                    mahasiswa = homeUiState.mahasiswa, modifier = modifier.fillMaxWidth(),
+                    onDetailClick = {
+                        onDetailClick(it.nim)
+                    },
+                    onDeleteClick = {
+                        onDeleteClick(it)
+                    }
+                )
+            }
+        is HomeUiState.Error -> OnError(retryAction, modifier = Modifier.fillMaxSize())
+    }
+}
+
 
 @Composable
 fun OnLoading(modifier: Modifier = Modifier){
