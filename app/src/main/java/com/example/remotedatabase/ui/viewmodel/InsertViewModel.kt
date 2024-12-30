e82aa6a76acf979cd3f7f2f8c88d468ebc9992ab
andiharun29 <andiharunalrasyid14@gmail.com>
2024-12-30 10:45:49 +0700

    Menambahkan data class InsertUiEvent untuk representasi data input mahasiswa

--- app/src/main/java/com/example/remotedatabase/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/remotedatabase/ui/viewmodel/InsertViewModel.kt
@@ -11,6 +11,15 @@ import kotlinx.coroutines.launch
 
 
 
+data class InsertUiEvent(
+    val nim: String = "",
+    val nama: String = "",
+    val alamat: String = "",
+    val jeniskelamin: String = "",
+    val kelas: String = "",
+    val angkatan: String = ""
+)
+
 fun InsertUiEvent.toMahasiswa(): Mahasiswa = Mahasiswa(
     nim = nim,
     nama = nama,
