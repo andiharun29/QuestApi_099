4dd558a365961fbc16f967bc6de71e188e0b7e1a
andiharun29 <andiharunalrasyid14@gmail.com>
2024-12-30 10:33:44 +0700

    Menambahkan implementasi NetworkMahasiswaRepository untuk menghubungkan dengan MahasiswaService

--- app/src/main/java/com/example/remotedatabase/Repository/MahasiswaRepository.kt
+++ app/src/main/java/com/example/remotedatabase/Repository/MahasiswaRepository.kt
@@ -16,3 +16,37 @@ interface MahasiswaRepository{
     suspend fun deleteMahasiswa(nim: String)
 }
 
+class NetworkMahasiswaRepository(
+    private val mahasiswaApiService: MahasiswaService
+): MahasiswaRepository {
+    override suspend fun insertMahasiswa(mahasiswa: Mahasiswa) {
+        mahasiswaApiService.insertMahasiswa(mahasiswa)
+    }
+
+    override suspend fun updateMahasiswa(nim: String, mahasiswa: Mahasiswa) {
+        mahasiswaApiService.updateMahasiswa(nim, mahasiswa)
+    }
+
+    override suspend fun deleteMahasiswa(nim: String) {
+        try {
+            val response = mahasiswaApiService.deleteMahasiswa(nim)
+            if (!response.isSuccessful) {
+                throw IOException(
+                    "Failed to delete mahasiswa. Http error code:" +
+                            "${response.code()}"
+                )
+            } else {
+                response.message()
+                println(response.message())
+            }
+        } catch (e: Exception) {
+            throw e
+        }
+    }
+
+    override suspend fun getMahasiswa(): List<Mahasiswa> = mahasiswaApiService.getMahasiswa()
+
+    override suspend fun getMahasiswaBynim(nim: String): Mahasiswa {
+        return mahasiswaApiService.getMahasiswaBynim(nim)
+    }
+}
